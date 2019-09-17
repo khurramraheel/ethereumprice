b8bff9644f6294ed2560e8a1e78f47b9b766eba0
Usama Asif <ranausama48@gmail.com>
2019-09-17 14:58:36 +0500

    create trade component

--- src/views/Tabs/Tabs.js
+++ src/views/Tabs/Tabs.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import Trade from '../Tabs/Trade/Trade'
 export default class Tabs extends Component {
     render() {
         return (
@@ -17,7 +17,7 @@ export default class Tabs extends Component {
                 </ul>
                 <div class="tab-content p-3" id="myTabContent">
                     <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">Consequat occaecat ullamco amet non eiusmod nostrud dolore irure incididunt est duis anim sunt officia. Fugiat velit proident aliquip nisi incididunt nostrud exercitation proident est nisi. Irure magna elit commodo anim ex veniam culpa eiusmod id nostrud sit cupidatat in veniam ad. Eiusmod consequat eu adipisicing minim anim aliquip cupidatat culpa excepteur quis. Occaecat sit eu exercitation irure Lorem incididunt nostrud.</div>
-                    <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">Ad pariatur nostrud pariatur exercitation ipsum ipsum culpa mollit commodo mollit ex. Aute sunt incididunt amet commodo est sint nisi deserunt pariatur do. Aliquip ex eiusmod voluptate exercitation cillum id incididunt elit sunt. Qui minim sit magna Lorem id et dolore velit Lorem amet exercitation duis deserunt. Anim id labore elit adipisicing ut in id occaecat pariatur ut ullamco ea tempor duis.</div>
+                    <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab"><Trade/></div>
                     <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">Est quis nulla laborum officia ad nisi ex nostrud culpa Lorem excepteur aliquip dolor aliqua irure ex. Nulla ut duis ipsum nisi elit fugiat commodo sunt reprehenderit laborum veniam eu veniam. Eiusmod minim exercitation fugiat irure ex labore incididunt do fugiat commodo aliquip sit id deserunt reprehenderit aliquip nostrud. Amet ex cupidatat excepteur aute veniam incididunt mollit cupidatat esse irure officia elit do ipsum ullamco Lorem. Ullamco ut ad minim do mollit labore ipsum laboris ipsum commodo sunt tempor enim incididunt. Commodo quis sunt dolore aliquip aute tempor irure magna enim minim reprehenderit.</div>
                 </div>
             </div>
---
+++ src/views/Tabs/Trade/Trade.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react'
+import './style.css'
+class Trade extends Component {
+    constructor(props)
+    {
+        super(props);
+        this.state={
+            oder_digit:""
+        }
+    }
+    chageHandler = (e)=>{
+        var name = e.target.name;
+        var value = e.target.value;
+        console.log(name,value)
+    }
+  render() {
+    
+    return (
+       
+        <div>
+            <div class="card text-center align-items-center " >
+                
+                <div class="card-body col-md-6 " style={{backgroundColor:'black !important'}}>    
+                    <div class="input-group mb-3" >
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="inputGroup-sizing-default">Current Balance PKR</span>
+                        </div>
+                        <input  type="text" 
+                            disabled
+                            className="form-control p-4 text-white border-0"
+                            aria-label="Sizing example input" 
+                            value="100"
+                            style={{ background: "#23272B", color: "white" }}
+                            aria-describedby="inputGroup-sizing-default"/>
+                    </div>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="inputGroup-sizing-default">Ethereum Price PKR</span>
+                        </div>
+                        <input  type="text" 
+                            disabled
+                            className="form-control p-4 text-white border-0"
+                            value="254"
+                            aria-label="Sizing example input" 
+                            style={{ background: "#23272B", color: "white" }}
+                            aria-describedby="inputGroup-sizing-default"/>
+                    </div>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text w-6" id="inputGroup-sizing-default">Buy Ethereum</span>
+                        </div>
+                        <input  type="text" 
+                            className="form-control p-4 text-white border-0"
+                            aria-label="Sizing example input" 
+                            style={{ background: "#23272B", color: "white" }}
+                            aria-describedby="inputGroup-sizing-default"/>
+                    </div> 
+                </div>
+                <div class="card-footer text-muted">
+                   
+                        <button
+                            className="input-grouptext btn btn-dark px-5 border-0"
+                            style={{ backgrund: "", color: "white", fontWeight: "600",textAlign:'right' }}
+                        >
+                        SUBMIT
+                        </button>
+                   
+                </div> 
+            </div>
+        </div>
+    )
+  }
+}
+
+export default Trade
