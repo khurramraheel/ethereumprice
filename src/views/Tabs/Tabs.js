8baeab431a8d8e3ab4a808b84f4bd6992d9b898b
naseebali_na2 <naseebalina2@gmail.com>
2019-09-17 12:56:22 +0500

    Tabs.js updated

--- src/views/Tabs/Tabs.js
+++ src/views/Tabs/Tabs.js
@@ -21,9 +21,9 @@ export default class Tabs extends Component {
                     <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab"><Transaction /></div>
                     <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">Ad pariatur nostrud pariatur exercitation ipsum ipsum culpa mollit commodo mollit ex. Aute sunt incididunt amet commodo est sint nisi deserunt pariatur do. Aliquip ex eiusmod voluptate exercitation cillum id incididunt elit sunt. Qui minim sit magna Lorem id et dolore velit Lorem amet exercitation duis deserunt. Anim id labore elit adipisicing ut in id occaecat pariatur ut ullamco ea tempor duis.</div>
                     <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
-				<Profile />
+                        <Profile />
                     </div>
-  					
+
                 </div>
             </div>
         )
