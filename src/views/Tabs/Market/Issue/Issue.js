1c9afa6db1bef7060ea1ed8b04962158ce812876
Usama Asif <ranausama48@gmail.com>
2019-10-02 12:21:12 +0500

    "New task UI designe"

---
+++ src/views/Tabs/Market/Issue/Issue.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Issue extends Component {
+  render() {
+    return (
+      <div>
+        Issue
+      </div>
+    )
+  }
+}
+
+export default Issue
